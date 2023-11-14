5e204af9950e7d70174c683b7ecd3843a45c50ad
Pedro <pedro@pepicrft.me>
2023-11-14 20:49:51 +0100

    Make the Atomics dependency dynamic

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -2,4 +2,6 @@ import ProjectDescription
 
 let dependencies = Dependencies(swiftPackageManager: .init([
     Package.remote(url: "https://github.com/vapor/vapor.git", requirement: .upToNextMinor(from: "4.83.1"))
+], productTypes: [
+    "Atomics": .framework
 ]))
